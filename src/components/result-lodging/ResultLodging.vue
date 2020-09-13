7420d63dbd4ad655b774b7284ced7896e658123b
Chicho_Casa <alvarez93lopez@gmail.com>
2020-09-12 22:09:23 -0400

    ...

--- src/components/result-lodging/ResultLodging.vue
+++ src/components/result-lodging/ResultLodging.vue
@@ -2,23 +2,72 @@
   <div id="content">
       <NavBar2 :menuLinks="menuLinks"></NavBar2>
       <Breadcrumb :elementList="breadcrumbList"></Breadcrumb>
-      <TwoColumn></TwoColumn>
+      <div id="twoColumn" class="custom-margin row">
+        <div class="col-lg-3 left-column-filter-wrapper">
+            <LeftColumnFilters></LeftColumnFilters>
+        </div>
+        <div class="col-lg-9 right-column-list-wrapper">
+            <RightColumnList :resultList="resultList" :filters="filters"></RightColumnList>
+        </div>
+        </div>
   </div>
 </template>
 
 <script>
 import NavBar2 from '../shared/NavBar2';
-import TwoColumn from '../result-lodging/TwoColumn';
+import LeftColumnFilters from './LeftColumnFilters';
+import RightColumnList from './RightColumnList';
 import Breadcrumb from '../shared/Breadcrumb';
+import moment from 'moment'
 
 export default {
     components: {
         NavBar2,
-        TwoColumn,
+        LeftColumnFilters,
+        RightColumnList,
         Breadcrumb
     },
     data(){
         return {
+            filters: {
+                selectedLodgingDestinyValue: '',
+                selectedArriveDate: moment(),
+                selectedDepartureDate: moment().add(1, 'days'),
+                selectedRoomLayout: {
+                    adults: {
+                        value: 2,
+                        display: 'Adulto(s)'
+                    },
+                    kids: {
+                        value: 1,
+                        display: 'Niño(s)'
+                    }
+                },
+                selectedRooms: {
+                    rooms: {
+                        value: 2,
+                        display: 'Habitación(es)'
+                    }
+                },
+                selectedNationality: null,
+            },
+            resultList:[
+                {
+                    images: [
+                        '/img/homelogin_destino_habana.jpg',
+                        '/img/home_servicios_restaurant.jpg',
+                        '/img/home_servicios_autos.jpg',
+                    ],
+                    name: 'Hotel Iberostar Grand Packard',
+                    stars: 5,
+                    location: 'La Habana, Cuba',
+                    mapLink: 'http://a.a',
+                    price: {
+                        value: 87.5,
+                        currency: 'USD'
+                    }
+                }
+            ],
             breadcrumbList: [
                 'Inicio',
                 'Alojamientos',
@@ -59,5 +108,11 @@ export default {
 </script>
 
 <style>
-
+    .left-column-filter-wrapper{
+        padding-left: 0;
+        padding-right: 30px;
+    }
+    .right-column-list-wrapper{
+        padding-right: 0;
+    }
 </style>
--- src/components/result-lodging/RightColumnList.vue
+++ src/components/result-lodging/RightColumnList.vue
@@ -1,18 +1,24 @@
 <template>
   <div id="right-column-list">
-
+    <ResultList :resultList="resultList" :filters="filters"></ResultList>
   </div>
 </template>
 
 <script>
+import ResultList from './ResultList';
 export default {
-
+  components: {
+    ResultList
+  },
+  props: {
+    resultList: Array,
+    filters: Object
+  }
 }
 </script>
 
 <style>
     #right-column-list{
-        background-color: green;
-        height: 100%;
+        /* height: 100%; */
     }
 </style>
