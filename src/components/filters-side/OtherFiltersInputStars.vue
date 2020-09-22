a4a416f99ce67b0a8ab181014b2b4506a356b0d0
Chicho_Casa <alvarez93lopez@gmail.com>
2020-09-21 22:44:42 -0400

    ...

--- src/components/filters-side/OtherFiltersInputStars.vue
+++ src/components/filters-side/OtherFiltersInputStars.vue
@@ -10,11 +10,11 @@
       <div class="input-content" v-show="isOpen">
           <div class="stars-line">
               <div class="stars-align">
-                <span class="star-button"><i class="mdi mdi-star"></i></span>
-                <span class="star-button"><i class="mdi mdi-star"></i></span>
-                <span class="star-button"><i class="mdi mdi-star"></i></span>
-                <span class="star-button"><i class="mdi mdi-star"></i></span>
-                <span class="star-button"><i class="mdi mdi-star"></i></span>
+                <span class="star-button" ref="star" data-value=1 @click="updateValue"><i class="mdi mdi-star"></i></span>
+                <span class="star-button" ref="star" data-value=2 @click="updateValue"><i class="mdi mdi-star"></i></span>
+                <span class="star-button" ref="star" data-value=3 @click="updateValue"><i class="mdi mdi-star"></i></span>
+                <span class="star-button" ref="star" data-value=4 @click="updateValue"><i class="mdi mdi-star"></i></span>
+                <span class="star-button" ref="star" data-value=5 @click="updateValue"><i class="mdi mdi-star"></i></span>
               </div>
           </div>
       </div>
@@ -29,16 +29,42 @@ export default {
         },
         placeholder: {
             default: 'Nombre'
+        },
+        value: {
+            type: Number,
+            default: 0
         }
+    },
+    mounted(){
+        this.currentValue = this.value
     },
     data(){
         return {
-            isOpen: true
+            isOpen: true,
+            currentValue: 0
         }
     },
     methods: {
         openClose(){
             this.isOpen = !this.isOpen
+        },
+        updateValue(event){
+            this.currentValue = event.currentTarget.getAttribute('data-value')
+            this.setActiveStars()
+            this.$emit('input',event.currentTarget.getAttribute('data-value'))
+        },
+        setActiveStars(){
+            let current = this.currentValue
+            document.getElementsByClassName('star-button').forEach(function(el)
+            {
+                let v = el.getAttribute('data-value')
+                if(v <= current){
+                    el.classList.add('active')
+                }
+                else{
+                    el.classList.remove('active')
+                }
+            })
         }
     }
 }
@@ -78,6 +104,12 @@ export default {
     }
     .input-wrapper .stars-line .star-button:hover{
         color: #6d6d6d;
+    }
+    .input-wrapper .stars-line .star-button.active{
+        color: #6d6d6d;
+    }
+    .input-wrapper .stars-line .star-button.active:hover{
+        color: #c4c4c4;
     }
 
 </style>
